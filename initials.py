8a1bf2b125322051d9f2012c230f0e9cbd18346e
Mathew-etsey <2425403142@live.gctu.edu.gh>
2026-05-30 17:15:55 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Printing out my name initials
+# Assignment Number: 2
+#
+# Name: AMEDZROVI MATHEW ETSEY
+# STUDENT ID: 2425403142
+# Email: mathewetseyamedzrovi@gmail.com
+# Grader: Augustus
+#
+# On my honor, Amedzrovi Mathew Etsey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Printing  my initials in large  letters.
 
+    print()
+    print("...AME")
+    print()
+
+    print("...AAAAAAAAAAAA........MMMM....MMMM........EEEEEEEEEEEE.....")
+    print("...AA........AA........MMMMM..MMMMM........EE...............")
+    print("...AA........AA........MM.MM..MM.MM........EE...............")
+    print("...AA........AA........MM..MMMM..MM........EE...............")
+    print("...AAAAAAAAAAAA........MM...MM...MM........EEEEEEEEEE.......")
+    print("...AA........AA........MM........MM........EE...............")
+    print("...AA........AA........MM........MM........EE...............")
+    print("...AA........AA........MM........MM........EE...............")
+    print("...AA........AA...**...MM........MM...**...EE.............**")
+    print("...AA........AA...**...MM........MM...**...EEEEEEEEEEEE...**")
+
+    print()
 
 
 main()
